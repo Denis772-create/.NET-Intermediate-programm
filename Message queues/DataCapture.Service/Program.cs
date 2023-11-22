9f849e57eda8a81e290845c7f53362f40873bb6f
Aliaksandrou, Dzianis <D.Aliaksandrou@wolterskluwer.com>
2023-11-22 14:32:31 +0500

    message queues HW tasks

---
+++ Message queues/DataCapture.Service/Program.cs
@@ -0,0 +1,74 @@
+﻿using RabbitMQ.Client;
+using System.Threading.Channels;
+
+namespace DataCapture.Service;
+
+internal class Program
+{
+    static void Main()
+    {
+        using var connection = GetRabbitMqConnection();
+        using var channel = ConfigureRabbitMqChanel(connection);
+
+        var pdfFiles = Directory.GetFiles("Data", "*.pdf");
+        foreach (var pdfFile in pdfFiles)
+        {
+            var fileBytes = File.ReadAllBytes(pdfFile);
+
+            var chunkSize = 1024 * 1024; // 1 MB
+            var totalChunks = (int)Math.Ceiling((double)fileBytes.Length / chunkSize);
+
+            // unique sequence identifier for this set of messages
+            var sequenceId = Guid.NewGuid().ToString();
+
+            // send data as a sequence of messages
+            for (var i = 0; i < totalChunks; i++)
+            {
+                var offset = i * chunkSize;
+                var remainingBytes = Math.Min(chunkSize, fileBytes.Length - offset);
+
+                var chunk = new byte[remainingBytes];
+                Buffer.BlockCopy(fileBytes, offset, chunk, 0, remainingBytes);
+
+                // mark each message with sequence identification fields
+                var properties = channel.CreateBasicProperties();
+                properties.Headers = new Dictionary<string, object>
+                {
+                    { "SequenceId", sequenceId },
+                    { "PositionId", i },
+                    { "SizeOrEnd", i == totalChunks - 1 ? totalChunks : 0 } // 0 if not the last, totalChunks if the last
+                };
+
+                channel.BasicPublish("data_capture_exchange", "data_capture_queue", properties, chunk);
+                Console.WriteLine($"Sent chunk {i + 1}/{totalChunks} to Processing service.");
+            }
+        }
+
+        channel.Close();
+        connection.Close();
+    }
+
+    static IConnection GetRabbitMqConnection()
+    {
+        var factory = new ConnectionFactory
+        {
+            Uri = new Uri("amqps://twqacgdc:DXaKVDPdSj-nYPyfaRpkQPLGeH_DeiRk@moose.rmq.cloudamqp.com/twqacgdc")
+        };
+        return factory.CreateConnection();
+    }
+
+    static IModel ConfigureRabbitMqChanel(IConnection connection)
+    {
+        var channel = connection.CreateModel();
+
+        channel.QueueDeclare("data_capture_queue", false, false, false);
+        channel.ExchangeDeclare("data_capture_exchange", ExchangeType.Direct,
+            arguments: new Dictionary<string, object>
+            {
+                { "x-max-length-bytes", 2097152 } // message limit 2 MB
+            });
+        channel.QueueBind("data_capture_queue", "data_capture_exchange", "data_capture_queue");
+
+        return channel;
+    }
+}
---
+++ Message queues/Processing.Service/Program.cs
@@ -0,0 +1,92 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Processing.Service;
+
+class ProcessingService
+{
+    // for this example we can store chunks in memory
+    private static readonly Dictionary<string, List<byte[]>> SequenceChunks = new();
+
+    static void Main()
+    {
+        using var connection = GetRabbitMqConnection();
+        using var channel = ConfigureRabbitMqChanel(connection);
+
+        // folder to store incoming messages
+        Directory.CreateDirectory("IncomingMessages");
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (_, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var properties = ea.BasicProperties;
+
+            var sequenceId = Encoding.UTF8.GetString((byte[])properties.Headers["SequenceId"]);
+            var positionId = (int)properties.Headers["PositionId"];
+            var sizeOrEnd = (int)properties.Headers["SizeOrEnd"];
+
+            Console.WriteLine($"Received chunk #{positionId + 1}. Last chunk: {sizeOrEnd != 0}");
+
+            if (!SequenceChunks.ContainsKey(sequenceId))
+            {
+                SequenceChunks[sequenceId] = new List<byte[]>();
+            }
+            SequenceChunks[sequenceId].Add(body);
+
+            // if the last chunk, assemble the complete data
+            if (positionId == sizeOrEnd - 1)
+            {
+                Console.WriteLine($"Assembling complete data for sequence {sequenceId}");
+                AssembleAndProcessData(sequenceId);
+            }
+        };
+        channel.BasicConsume("data_capture_queue", autoAck: true, consumer: consumer);
+
+        Console.ReadLine();
+        channel.Close();
+        connection.Close();
+    }
+
+    static void AssembleAndProcessData(string sequenceId)
+    {
+        if (SequenceChunks.ContainsKey(sequenceId))
+        {
+            var chunks = SequenceChunks[sequenceId];
+            var completeData = chunks.SelectMany(chunk => chunk).ToArray();
+
+            // save the complete data to a PDF file
+            var completeDataFilePath = $"IncomingMessages/{sequenceId}_complete_data.pdf";
+            File.WriteAllBytes(completeDataFilePath, completeData);
+            Console.WriteLine($"Processed complete data saved as PDF: {completeDataFilePath}");
+
+            // clear the chunks for the processed sequence
+            SequenceChunks.Remove(sequenceId);
+        }
+    }
+
+    static IConnection GetRabbitMqConnection()
+    {
+        var factory = new ConnectionFactory
+        {
+            Uri = new Uri("amqps://twqacgdc:DXaKVDPdSj-nYPyfaRpkQPLGeH_DeiRk@moose.rmq.cloudamqp.com/twqacgdc")
+        };
+        return factory.CreateConnection();
+    }
+
+    static IModel ConfigureRabbitMqChanel(IConnection connection)
+    {
+        var channel = connection.CreateModel();
+
+        channel.QueueDeclare("data_capture_queue", false, false, false);
+        channel.ExchangeDeclare("data_capture_exchange", ExchangeType.Direct,
+            arguments: new Dictionary<string, object>
+            {
+                { "x-max-length-bytes", 2097152 } // message limit 2 MB
+            });
+        channel.QueueBind("data_capture_queue", "data_capture_exchange", "data_capture_queue");
+
+        return channel;
+    }
+}
